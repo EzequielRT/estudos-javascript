f2920d3db775ec28e4eea0419764b366f70e2b30
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-02 08:06:55 -0300

    Manipulando Class do elemento (1/2)

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,1 +1,15 @@
-document.getElementById('exemplo').innerHTML = "Paola <button>Botão</button>";
+function verde() {
+    document.querySelector('#exemplo').classList.remove('vermelho');
+    document.querySelector('#exemplo').classList.remove('azul');
+    document.querySelector('#exemplo').classList.add('verde');
+}
+function vermelho() {
+    document.querySelector('#exemplo').classList.remove('verde');
+    document.querySelector('#exemplo').classList.remove('azul');
+    document.querySelector('#exemplo').classList.add('vermelho');
+}
+function azul() {
+    document.querySelector('#exemplo').classList.remove('vermelho');
+    document.querySelector('#exemplo').classList.remove('verde');
+    document.querySelector('#exemplo').classList.add('azul');
+}
