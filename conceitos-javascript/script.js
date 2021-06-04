e7b955d1cf3c03df9a2d4db81c6a952df9c526c8
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-03 23:52:22 -0300

    Evento de Clique

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,20 +1,3 @@
-function subirTela() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+function clicou() {
+    alert("Você clicou neste botão");
 }
-
-function decidirBotaoScroll() {
-    if (window.scrollY === 0) {
-        // oculta botao
-        document.querySelector('.scroll-button').style.display = 'none';
-    } else {
-        // mostra botao
-        document.querySelector('.scroll-button').style.display = 'block';
-    }
-}
-
-//setInterval(decidirBotaoScroll, 500); // verifica posicao do scroll a cada meio segundo
-
-window.addEventListener('scroll', decidirBotaoScroll); // monitora o scroll
