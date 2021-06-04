576e1c443824f4f878ad08b342fe79815d62d428
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-03 21:57:24 -0300

    Manipulando Atributos

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,14 +1,8 @@
-function alterar(titulo) {
-    document.getElementById('titulo').innerHTML = titulo;
-    document.getElementById('campo').value = titulo;
+function trocarImagem(filename, animalname) {
+    document.querySelector('.imagem').setAttribute('src', 'images/'+filename);
+    document.querySelector('.imagem').setAttribute('data-animal', animalname);
 }
-
-function somar (x, y) {
-    let total = x + y;
-
-    return total;
+function pegarAnimal () {
+    let animal = document.querySelector('.imagem').getAttribute('data-animal');
+    alert("O animal é: " + animal);
 }
-
-var resultado = somar(10, 15);
-
-console.log(resultado)
