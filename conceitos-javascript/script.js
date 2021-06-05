5d1c67942094e5afb8c3813e986d7a1880e970a9
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-05 18:09:57 -0300

    Arrays

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,31 +1,14 @@
-function azul () {
-    limpar();
-    document.getElementById('titulo').classList.add('azul');
-}
-function vermelho () {
-    limpar();
-    document.getElementById('titulo').classList.add('vermelho');
-}
-function verde () {
-    limpar();
-    document.getElementById('titulo').classList.add('verde');
-}
-function limpar() {
-    document.getElementById('titulo').classList.remove('azul');
-    document.getElementById('titulo').classList.remove('vermelho');
-    document.getElementById('titulo').classList.remove('verde');
-}
-function mostrarTelefone(elemento) {
-    elemento.style.display = 'none';
-    document.getElementById('telefone').style.display = 'block';
-}
-function ocultarTexto() {
-    document.getElementById('texto').style.display = 'none';
-    document.getElementById('ocultar').style.display = 'none';
-    document.getElementById('mostrar').style.display = 'block';
-}
-function mostrarTexto() {
-    document.getElementById('texto').style.display = 'block';
-    document.getElementById('ocultar').style.display = 'block';    
-    document.getElementById('mostrar').style.display = 'none';
-}
+let carros = [
+    'Palio', 
+    'Uno', 
+    'Corolla', 
+    'Ferrari'
+];
+
+let ingredientes = [
+    ['uva', 'pera', 'maça'],
+    ['arroz', 'macarrão']
+]
+
+console.log(ingredientes[0][2]);
+console.log(ingredientes[1][1]);
