7472e96a4f0bc6dcf77b31b08bd2976ed4c2ba95
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-05 18:41:23 -0300

    Objeto (2/2)

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -9,11 +9,29 @@ let carro = { // objeto
     marca:'Fiat',
     modelo:'Uno',
     peso:'800kg',
+    ligado:false,
     ligar:function() {
+        this.ligado = true;
+        console.log("Ligando o "+this.modelo);
         console.log("VRUM VRUM");
+    },
+    acelerar:function() {
+        if (this.ligado == true) {
+        console.log("Riihhhhh");
+        } else {
+            console.log(this.marca+" "+this.modelo+" não está ligado!");
+        }
     }
 };
 
-console.log(carro['marca']);
 console.log(carro.modelo);
-carro.ligar();
+carro.acelerar();
+
+let motos = [
+    {marca:'Honda', modelo:'cg 150'},
+    {marca:'Yamaha', modelo:'Crosser 150'},
+    {marca:'BMW', modelo:'GS 1200'}
+];
+
+console.log(motos[2].modelo);
+console.log(motos[1].modelo);
