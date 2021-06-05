c5e09bf6d01d65b89b3e679bf790c3ef704dea27
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-04 23:44:35 -0300

    Mudança de estilos

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,7 +1,31 @@
-function digitou(evento) {
-    if (evento.keyCode === 13 && evento.ctrlKey === true) { // tecla Enter
-        let texto = document.getElementById('campo').value;
-
-        console.log(texto);
-    }
+function azul () {
+    limpar();
+    document.getElementById('titulo').classList.add('azul');
+}
+function vermelho () {
+    limpar();
+    document.getElementById('titulo').classList.add('vermelho');
+}
+function verde () {
+    limpar();
+    document.getElementById('titulo').classList.add('verde');
+}
+function limpar() {
+    document.getElementById('titulo').classList.remove('azul');
+    document.getElementById('titulo').classList.remove('vermelho');
+    document.getElementById('titulo').classList.remove('verde');
+}
+function mostrarTelefone(elemento) {
+    elemento.style.display = 'none';
+    document.getElementById('telefone').style.display = 'block';
+}
+function ocultarTexto() {
+    document.getElementById('texto').style.display = 'none';
+    document.getElementById('ocultar').style.display = 'none';
+    document.getElementById('mostrar').style.display = 'block';
+}
+function mostrarTexto() {
+    document.getElementById('texto').style.display = 'block';
+    document.getElementById('ocultar').style.display = 'block';    
+    document.getElementById('mostrar').style.display = 'none';
 }
