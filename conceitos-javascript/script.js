7dc095dd98ba578199380c281d519cce08a3a643
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-06 19:05:31 -0300

    Loop For

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,39 +1,19 @@
-let dia = 7;
-let diaNome = null;
-let fimDeSemana = null;
+/*let texto = ''; // FOR LOOP
 
-switch (dia) {
-    case 1:
-        diaNome = "segunda-feira";
-        break;
-    case 2:
-        diaNome = "terça-feira";
-        break;
-    case 3:
-        diaNome = "quarta-feira";
-        break;
-    case 4:
-        diaNome = "quinta-feira";
-        break;
-    case 5:
-        diaNome = "sexta-feira";
-        break;
-    case 6:
-        diaNome = "sábado";
-        break;
-    case 7:
-        diaNome = "domingo";
-        break;
+for (let i = 0; i < 50; i++) {
+    texto = texto + i + '<br>';
+}
+document.getElementById('demo').innerHTML = texto;*/
+
+
+let carros = [ 'Ferrari', 'Fusca', 'Palio', 'Corolla']; // FOR LOOP ARRAY
+
+let html = '<ul>';
+
+for (let i in carros) {
+    html = html + '<li>' + carros[i] + '</li>';
 }
 
-switch (dia) {
-    case 6:
-    case 7:
-        fimDeSemana = "fim de semana.";
-        break;
-    default:
-        fimDeSemana = "dia de semana.";
-}
+html += '</ul>';
 
-
-document.getElementById('dia').innerHTML = "Hoje é "+diaNome+", "+fimDeSemana;
+document.getElementById('demo').innerHTML = html;
