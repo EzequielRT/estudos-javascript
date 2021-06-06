89f3ab87547b73330838ed8329f59b35ca7c858d
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-06 20:04:36 -0300

    Métodos de String (2/3)

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,14 +1,8 @@
 let nome = 'Ezequiel Rodrigues Trindade';
 
-let resultado1 = nome.length;
-let resultado2 = nome.indexOf('Rodrigues');
-
-let resultado3 = '';
-if (nome.indexOf('T') > -1) {
-    resultado3 = 'Achou!';
-}else {
-    resultado3 = 'Não achou!';
-}
+let resultado1 = nome.slice(-8); // Pode começar a contagem decrescente
+let resultado2 = nome.substring(0, 8); // Nao pode começar a contagem decrescente
+let resultado3 = nome.substr(9, 9); // Primeiro parametro é o inicio e o segundo é a qtd de caracteres selecionados
 
 console.log(resultado1);
 console.log(resultado2);
