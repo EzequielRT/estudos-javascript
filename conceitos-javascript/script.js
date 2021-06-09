ecd16ad9984fda367002725808483221da8994df
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-09 01:18:21 -0300

    Metodo Some - testa se algum item atende ao parametro

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,7 +1,7 @@
 let lista = [45, 4, 9, 16, 25];
 let lista2 = [];
 
-lista2 = lista.every(function (item) { // Retorna TRUE somente se todos os itens estiverem dentro do parametro
+lista2 = lista.some(function (item) { // Retorna TRUE se algum dos itens estiverem dentro do parametro
     if (item > 20) {
         return true;
     } else {
