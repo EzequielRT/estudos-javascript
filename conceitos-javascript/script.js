abc36884268c7ec55462f7e6f800e69173f30756
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-09 01:26:20 -0300

    Metodo Find - busca e retorna item ou posicao no Array

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,8 +1,12 @@
 let lista = [45, 4, 9, 16, 25];
 let lista2 = [];
 
-lista2 = lista.some(function (item) { 
-    return (item > 50)? true : false; // Operador ternario
+lista2 = lista.find(function (item) {
+    return (item === 16) ? true : false;  // Busca e retorna o item do Array
+});
+
+lista2 = lista.findIndex(function (item) { // Busca e retorna a posicao do item no Array
+    return (item === 16) ? true : false;
 });
 
 let res = lista2;
