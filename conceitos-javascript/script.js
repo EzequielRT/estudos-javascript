8bdd49f6a5df9c444ff7092b53eab6029e781502
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-09 01:17:13 -0300

    Metodo Every - testa se todos itens atendem ao parametro

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,8 +1,8 @@
 let lista = [45, 4, 9, 16, 25];
 let lista2 = [];
 
-lista2 = lista.filter(function (item) { // Filtrando itens do array com Filter
-    if (item < 20) {
+lista2 = lista.every(function (item) { // Retorna TRUE somente se todos os itens estiverem dentro do parametro
+    if (item > 20) {
         return true;
     } else {
         return false;
