9582dd3a9c9c6e9aac95b4515095dee440f57f60
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-09 00:53:27 -0300

    Remove itens do Array

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,6 +1,6 @@
 let lista = ['Ovo', 'Farinha', 'Corante', 'Massa'];
 
-lista.push('Prato'); // Adiciona item ao Array
+lista.splice(0, 2); // Removendo itens do Array - primeiro parametro é a partir de qual item sera removido e o segundo é a qtd de itens
 
 let res = lista;
 
