d0ca73c5c06664cf8a9cd99e00d6cf583223e415
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-09 00:59:35 -0300

    Ordenando Array por ordem inversa

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,7 +1,6 @@
 let lista = ['Ovo', 'Farinha', 'Corante', 'Massa'];
 
-lista.sort(); // Ordenando Array por ordem alfabetica
-lista.reverse(); // Ordenando Array por ordem alfabetica decrescente
+lista.reverse(); // Ordenando Array por ordem inversa
 
 let res = lista;
 
