c04aac2c43d9b0e2bf888c88391bbe12baeb6841
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-08 23:18:11 -0300

    Substituicao de palavras com replace

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,9 +1,3 @@
 let nome = 'Ezequiel Rodrigues Trindade';
-
-let resultado1 = nome.slice(-8); // Pode começar a contagem decrescente
-let resultado2 = nome.substring(0, 8); // Nao pode começar a contagem decrescente
-let resultado3 = nome.substr(9, 9); // Primeiro parametro é o inicio e o segundo é a qtd de caracteres selecionados
-
-console.log(resultado1);
-console.log(resultado2);
-console.log(resultado3);
+let resultado = nome.replace('Rodrigues', 'Moura'); // Substitucao de palavras
+console.log(resultado);
