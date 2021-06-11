eaca9ce067bf2ff8546892be1c96d5febe4107d7
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-10 22:25:20 -0300

    Manipulando DATA

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,3 +1,10 @@
-let novoValor = Date.now(); // Retorna a data atual em TIMESTAMP
+let d = new Date();
 
+//d.setFullYear(2022); // Seta o ano = 2022
+//d.setMonth(11); // Seta o mes = Dez
+//d.setDate(15); // Seta o dia
+//d.setDate(d.getDate() + 30); // Aumenta 30 dias
+d.setHours(d.getHours() - 6) // Diminui 6 horas
+
+let novoValor = d;
 console.log(novoValor);
