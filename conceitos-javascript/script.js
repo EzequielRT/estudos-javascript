8ce8d351ce48e9880c52c4a6fdb33e62de0c1fcd
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-10 21:23:31 -0300

    Metodos para retornar informacoes da DATA

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,6 +1,13 @@
 let d = new Date();
 
-console.log(d);
-console.log(d.toString());
-console.log(d.toDateString());
-console.log(d.toUTCString());
+//let novoValor = d.getFullYear(); // Retorna o ano = 2021
+//let novoValor = d.getMonth(); // Retorna o mes = 8
+//let novoValor = d.getDay(); // Retorna o dia da semana (vai de 0 a 6) = 4
+//let novoValor = d.getDate(); // Retorna o dia atual = 10
+//let novoValor = d.getHours(); // Retorna a hora = 21
+//let novoValor = d.getMinutes(); // Retorna os minutos = 13
+//let novoValor = d.getSeconds(); // Retorna os segundos = 11
+//let novoValor = d.getMilliseconds(); // Retorna os milisegundos
+//let novoValor = d.getTime(); // Qtd de milisegundos a partir de 01/01/1970 00:00
+
+console.log(novoValor);
