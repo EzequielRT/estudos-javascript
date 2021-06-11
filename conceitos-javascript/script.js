0888769093ae7d52c7dc994cb78c6934c62a62c1
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-10 21:26:58 -0300

    Retornando data atual em TIMESTAMP com Date.now()

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,13 +1,3 @@
-let d = new Date();
-
-//let novoValor = d.getFullYear(); // Retorna o ano = 2021
-//let novoValor = d.getMonth(); // Retorna o mes = 8
-//let novoValor = d.getDay(); // Retorna o dia da semana (vai de 0 a 6) = 4
-//let novoValor = d.getDate(); // Retorna o dia atual = 10
-//let novoValor = d.getHours(); // Retorna a hora = 21
-//let novoValor = d.getMinutes(); // Retorna os minutos = 13
-//let novoValor = d.getSeconds(); // Retorna os segundos = 11
-//let novoValor = d.getMilliseconds(); // Retorna os milisegundos
-//let novoValor = d.getTime(); // Qtd de milisegundos a partir de 01/01/1970 00:00
+let novoValor = Date.now(); // Retorna a data atual em TIMESTAMP
 
 console.log(novoValor);
