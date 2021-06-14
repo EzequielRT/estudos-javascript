ff10a03a22dd699d230161845a1519a3cf8963bc
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-14 01:09:32 -0300

    Timer setTimeout - roda apos determinados seg

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,19 +1,10 @@
-function horaAtual() {
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-    let txt = h+':'+m+':'+s;
-
-    document.querySelector('#demo').innerHTML = txt; // Funcao para retornar a hora atual
-}
-
 let timer;
-
-function comecar() {
-    timer = setInterval(horaAtual, 1000); // Funcao para retornar a funcao horaAtual a cada 1seg
+function rodar() {
+    timer = setTimeout(function() {
+        document.querySelector('#demo').innerHTML = 'Rodou!'; // Espera 2s e executa a funcao rodar
+    }, 2000);
 }
 
 function parar() {
-    clearInterval(timer); // Limpa o intervalo da variavel timer
+    clearTimeout(timer);
 }
