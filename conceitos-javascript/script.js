8f4d923058b5ebb90b0f8cca13e879bbb66b9e3b
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-13 23:52:44 -0300

    Timers com Javascript - ex. funcao para retornar hora atual

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,20 +1,19 @@
-//let novoValor = Math.PI; // PI = 3.141592653589793
+function horaAtual() {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let txt = h+':'+m+':'+s;
 
-//let novoValor = Math.round(3.67); // Arredonda dependendo do numero = 4
-//let novoValor = Math.round(3.47); // Arredonda dependendo do numero = 3
+    document.querySelector('#demo').innerHTML = txt; // Funcao para retornar a hora atual
+}
 
-//let novoValor = Math.floor(9.97); // Arredonda sempre para baixo = 9
+let timer;
 
-//let novoValor = Math.ceil(7.41); // Arredonda sempre para cima = 8
+function comecar() {
+    timer = setInterval(horaAtual, 1000); // Funcao para retornar a funcao horaAtual a cada 1seg
+}
 
-//let novoValor = Math.abs(-9.97); // Retorna o numero absoluto = 9.97
-
-//let novoValor = Math.min(7, 100, 600, 20, 3); // Retorna o menor numero = 3
-
-//let novoValor = Math.max(7, 100, 600, 20, 3); // Retorna o maior numero = 600
-
-//let novoValor = Math.random(); // Retorna um numero aleatorio entre 0 e 1
-
-//let novoValor = Math.floor(Math.random() * 50); // Retorna um numero aleatorio entre 0 e 50;
-
-console.log(novoValor);
+function parar() {
+    clearInterval(timer); // Limpa o intervalo da variavel timer
+}
