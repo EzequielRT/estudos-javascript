93c2bb02735c0e5b3935ea6a9a79304578fbaff6
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-13 23:19:22 -0300

    Funcoes matematicas da classe Math

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,10 +1,20 @@
-let d = new Date();
+//let novoValor = Math.PI; // PI = 3.141592653589793
 
-//d.setFullYear(2022); // Seta o ano = 2022
-//d.setMonth(11); // Seta o mes = Dez
-//d.setDate(15); // Seta o dia
-//d.setDate(d.getDate() + 30); // Aumenta 30 dias
-d.setHours(d.getHours() - 6) // Diminui 6 horas
+//let novoValor = Math.round(3.67); // Arredonda dependendo do numero = 4
+//let novoValor = Math.round(3.47); // Arredonda dependendo do numero = 3
 
-let novoValor = d;
+//let novoValor = Math.floor(9.97); // Arredonda sempre para baixo = 9
+
+//let novoValor = Math.ceil(7.41); // Arredonda sempre para cima = 8
+
+//let novoValor = Math.abs(-9.97); // Retorna o numero absoluto = 9.97
+
+//let novoValor = Math.min(7, 100, 600, 20, 3); // Retorna o menor numero = 3
+
+//let novoValor = Math.max(7, 100, 600, 20, 3); // Retorna o maior numero = 600
+
+//let novoValor = Math.random(); // Retorna um numero aleatorio entre 0 e 1
+
+//let novoValor = Math.floor(Math.random() * 50); // Retorna um numero aleatorio entre 0 e 50;
+
 console.log(novoValor);
