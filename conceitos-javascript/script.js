55aff698eaeeef4aa7ff848da0738b36822c6865
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-14 22:49:05 -0300

    Retornando objetos que estao dentro de objetos

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -11,20 +11,5 @@ let pessoa = {
     },
 };
 
-//Desconstruindo objeto ex1
-/*let nome = pesssoa.nome;
-let sobrenome = pesssoa.sobrenome;
-let idade = pesssoa.idade;
-console.log(nome, sobrenome, idade);*/
-
-//Desconstruindo objeto ex2
-/*const {nome, sobrenome, idade} = pessoa;
-console.log(nome, sobrenome, idade);*/
-
-//Desconstruindo objeto ex3
-/*const {nome:pessoaNome, sobrenome, idade} = pessoa; // Troca o nome da variavel
-console.log(pessoaNome, sobrenome, idade);*/
-
-//Desconstruindo objeto ex4
-const {nome, sobrenome, apelido = 'n/d'} = pessoa; // Caso o objeto nn tenha o valor define o padrao
-console.log(nome, sobrenome, apelido);
+const {facebook, instagram} = pessoa.social;
+console.log(facebook, instagram);
