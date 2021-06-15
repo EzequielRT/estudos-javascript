8a0b9818d9909e8dee95a63437df1609c83a71bd
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-14 22:46:44 -0300

    Descontruindo Objetos (1/2)

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,8 +1,30 @@
-let nome = 'Ezequiel';
-let idade = 21;
+let pessoa = {
+    nome: "Ezequiel",
+    sobrenome: "Rodrigues",
+    idade: 90,
+    social:{
+        facebook:"Ezequiel Rodrigues",
+        instagram:"Zek_rodrigues"
+    },
+    nomeCompleto: function () {
+        return `${this.nome} ${this.sobrenome}`;
+    },
+};
 
-//let frase = 'Meu nome é '+nome+', e eu tenho '+idade+' anos e ano que vem eu farei '+(idade+1)+' anos.';
+//Desconstruindo objeto ex1
+/*let nome = pesssoa.nome;
+let sobrenome = pesssoa.sobrenome;
+let idade = pesssoa.idade;
+console.log(nome, sobrenome, idade);*/
 
-let frase = `Meu nome é ${nome}, e eu tenho ${idade} anos e ano que vem eu farei ${idade+1} anos.`;
+//Desconstruindo objeto ex2
+/*const {nome, sobrenome, idade} = pessoa;
+console.log(nome, sobrenome, idade);*/
 
-console.log(frase);
+//Desconstruindo objeto ex3
+/*const {nome:pessoaNome, sobrenome, idade} = pessoa; // Troca o nome da variavel
+console.log(pessoaNome, sobrenome, idade);*/
+
+//Desconstruindo objeto ex4
+const {nome, sobrenome, apelido = 'n/d'} = pessoa; // Caso o objeto nn tenha o valor define o padrao
+console.log(nome, sobrenome, apelido);
