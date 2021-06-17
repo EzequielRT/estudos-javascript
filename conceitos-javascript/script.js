fc35daf21786b2c1cfe06132990b61b12447cfe2
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-16 23:42:49 -0300

    Arrow Functions

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,15 +1,47 @@
+// Function 1º metodo
 
-// Desconstruindo Array Ex 02
-let [nome, sobrenome, idade=20] = ['Ezequiel', 'Rodrigues']; // = Ezequiel Rodrigues 20
-console.log(nome, sobrenome, idade);
-
-
-// Criando Array com Function e descontruindo
-
-function criar() {
-    let a = [1,2,3];
-    return a;
+/*function somar(x, y) {
+    return x + y;
 }
+console.log(somar(10, 5));*/
 
-let [a,b,c] = criar();
-console.log(a,b,c); // = 1, 2, 3
+
+
+// Function 2º metodo
+
+/*let somar = function(x, y) {
+    return x + y;
+}
+console.log(somar(10, 5));*/
+
+
+
+// Arrow functions
+
+//Ex 01
+/*let somar = (x, y) => {
+    return x + y;
+}
+console.log(somar(10, 5));*/
+
+
+//Ex 02
+/*let somar = (x, y) => x + y;
+console.log(somar(10, 5));*/
+
+
+//Ex 03
+/*let letrasNome = (nome) => {
+    return nome.length;
+}
+console.log(letrasNome("Ezequiel"));*/
+
+
+//Ex 04
+/*let letrasNome = (nome) => nome.length;
+console.log(letrasNome("Ezequiel"));*/
+
+
+//Ex 05
+let letrasNome = nome => nome.length;
+console.log(letrasNome("Ezequiel"));
