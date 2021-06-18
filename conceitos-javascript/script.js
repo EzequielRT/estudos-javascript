c9ab4de3e61ccfb4eae8f042b9caeefde7d981dd
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-18 00:27:17 -0300

    padStart e padEnd

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,17 +1,13 @@
-let lista = [1,2,3,4];
+let telefone = '5546';
 
-console.log(Object.keys(lista)); // Retorna os indices do array
-console.log(Object.values(lista)); // Retorna os valores do array
-console.log(Object.entries(lista)); // Retorna um array com o indice e seu valor
-
+console.log(telefone.padEnd(9, '*')); // = 5546*****
+console.log(telefone.padStart(9, '*')); // *****5546
 
 
-let pessoa = {
-    nome:'Ezequiel',
-    sobrenome:'Rodrigues',
-    idade:90
-}
 
-console.log(Object.keys(pessoa)); // ["nome", "sobrenome", "idade"]
-console.log(Object.values(pessoa)); // ["Ezequiel", "Rodrigues", 90]
-console.log(Object.entries(pessoa)); // Retorna um array com chave e valor [Array(2), Array(2), Array(2)]
+// Exemplo
+let cartao = '1234115161564511';
+let lastDigits = cartao.slice(-4); // = 4511
+let cartaoMascarado = lastDigits.padStart(16, '*');
+
+console.log(`Este é seu cartão? ${cartaoMascarado}`);
