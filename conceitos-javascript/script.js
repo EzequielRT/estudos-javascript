22723c11329c5c04bfa0136df0e8d72332d953c1
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-17 23:59:37 -0300

    Objeto: Key, Values e Entries

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,11 +1,17 @@
-let lista = ["ovo", "café", "arroz", "feijão", "macarrao "];
+let lista = [1,2,3,4];
 
-// Funcao Includes
-console.log(lista.includes("ovo")); // = true
-console.log(lista.includes("carne")); // = false
+console.log(Object.keys(lista)); // Retorna os indices do array
+console.log(Object.values(lista)); // Retorna os valores do array
+console.log(Object.entries(lista)); // Retorna um array com o indice e seu valor
 
-// Funcao Repeat
-console.log('x'.repeat(20)); // Repete 20x o x
 
-let nome = "Ezequiel ";
-console.log(nome.repeat(3)); // Repete 03x a variavel nome
+
+let pessoa = {
+    nome:'Ezequiel',
+    sobrenome:'Rodrigues',
+    idade:90
+}
+
+console.log(Object.keys(pessoa)); // ["nome", "sobrenome", "idade"]
+console.log(Object.values(pessoa)); // ["Ezequiel", "Rodrigues", 90]
+console.log(Object.entries(pessoa)); // Retorna um array com chave e valor [Array(2), Array(2), Array(2)]
