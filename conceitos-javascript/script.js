fbe56f63900576e47e9c18823bf595c579b8f79a
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-18 23:33:50 -0300

    JSON.parse

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,28 +1,5 @@
-//JSON
+// Transforma uma string em um JSON
 
-//EX  01
-let pessoa = {
-    nome:"Ezequiel",
-    idade:90
-};
-console.log(pessoa.nome, pessoa.idade);
+let pessoa = JSON.parse('{"nome":"Matheus", "idade":23, "estetica": {"altura":1.90, "peso":80}}');
 
-
-//EX 02
-let pessoa2 = {
-    nome:"Paola",
-    idade:22,
-    caracteristicas: ["sorridente", "maravilhosa", "top"] // Array de objeto
-};
-console.log(pessoa2.nome, pessoa2.idade, pessoa2.caracteristicas);
-
-//EX 03
-let pessoa3 = {
-    nome:"Matheus",
-    idade:23,
-    estetica: {
-        altura:1.90, // Objeto dentro de objeto
-        peso:80
-    }
-};
-console.log(pessoa3.nome, pessoa3.idade, pessoa3.estetica.altura, pessoa3.estetica.peso);
+console.log(pessoa.nome, pessoa.estetica.altura);
