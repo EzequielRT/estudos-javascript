86eb0a2cd410703ef2a0205b28d48c9cb7fd7e36
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-19 21:42:12 -0300

    Montando blog dinamicamente apos consumo de API

--- consumindo-api/script.js
+++ consumindo-api/script.js
@@ -6,9 +6,21 @@ function loadPosts() {
             return resultado.json();
         })
         .then(function(json) {
-            document.getElementById("posts").innerHTML = json.length+" posts.";
+            montarBlog(json);
         })
         .catch(function(error) {
             console.log("Deu problema!");
         });
 }
+
+function montarBlog(lista) {
+    let html = '';
+
+    for(let i in lista) {
+        html += "<h3>"+lista[i].title+"</h3>";
+        html += lista[i].body+"<br>";
+        html += "<hr/>";
+    }
+
+    document.getElementById("posts").innerHTML = html;
+};
