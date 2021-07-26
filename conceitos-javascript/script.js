17ff82ddc5f2c48b67908a203c8d2afe96a8f9ae
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-25 23:58:43 -0300

    Revendo os conceitos de Promisses

--- conceitos-javascript/script.js
+++ conceitos-javascript/script.js
@@ -1,12 +1,19 @@
-import { Animal, Gato } from './animal.js';
+let fazer = ()=>{
+    let promise = new Promise((resolve, reject)=>{
 
-let gato1 = new Animal('Gato', 4);
-console.log( gato1.falar('Miau') );
-console.log( gato1.dados );
+        console.log('Aguarde...');
 
+        setTimeout(()=>{
+            resolve('OK')
+        }, 3000);
 
-let gato2 = new Gato(4, 'Preto');
-console.log( gato2.dados );
-console.log( `Cor: ${gato2.getCor()}` );
+    });
 
+    return promise;
+};
 
+fazer()
+    .then((resposta)=>{
+        console.clear();
+        console.log(`Resposta: ${resposta}`);
+    });
